8b96ad4b11ede2db9836e0b3aa7d440f033d5e99
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-17 03:00:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   smallest = 0
   key = ""
   name_hash.each{ |name,score|
-    if smallest < score
+    if smallest > score
       smallest = score
       key = name
     end
