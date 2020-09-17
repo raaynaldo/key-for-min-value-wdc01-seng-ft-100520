70868c65417bfd46ec3dcc59910138fdbfe0dbab
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-17 03:05:51 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  smallest = name_hash.first
+  smallest = name_hash.default
   key = ""
   name_hash.each{ |name,score|
     if smallest < score
